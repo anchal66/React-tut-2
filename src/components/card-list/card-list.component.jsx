e6951845bb0f939c4d1b24217455592edbb4bb4f
Avinash Anchal <anchalavinash66@gmail.com>
2020-05-06 22:14:53 +0530

    letting card componet to list all csrds

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,7 +4,9 @@ import './card-list.style.css'
 export const CardList = (props) =>{
     return(
         <div className="card-list">
-            {props.children}
+            {props.monsters.map(monster =>
+            <h1 key={monster.id}>{monster.name}</h1>
+          )}
         </div>
     )
 }
