d96ccc50be5202c398b8277c8c327aa49620b4b1
Avinash Anchal <anchalavinash66@gmail.com>
2020-05-06 22:40:47 +0530

    Implented new compone for indisual card

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,12 @@
 import React from'react';
 import './card-list.style.css'
+import { Card } from '../card/card.component';
 
 export const CardList = (props) =>{
     return(
         <div className="card-list">
             {props.monsters.map(monster =>
-            <h1 key={monster.id}>{monster.name}</h1>
+            <Card  key={monster.id} monster={monster}/>
           )}
         </div>
     )
